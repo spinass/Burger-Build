462ac59ee8d0039e2f31bd0a11e8d3498e4cb308
Giedrius <giedrius@spinas.lt>
2019-12-11 16:28:03 +0200

    Dinamika ingredijentu pasirinkimui

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,18 @@ import "./Burger.css";
 import BurgerIngredient from "./Burgeringredient/Burgeringredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Prasau pasirinkti ingredijentus!</p>;
+  }
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
