54520bd5954f43844a4f522feae027d8c5d35fa2
Giedrius <giedrius@spinas.lt>
2019-12-12 12:03:09 +0200

    Turime meniu

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 0,
       bacon: 0,
-      cheese: 1,
+      cheese: 0,
       meat: 0
     }
   };
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Burger Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
