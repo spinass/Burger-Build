6859cff14c388cfddb85272807a4dd68400ff077
Giedrius <giedrius@spinas.lt>
2019-12-12 15:16:47 +0200

    pridedam Less ir Price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,11 +13,16 @@ const controls = [
 
 const buildControls = props => (
   <div className="BuildControls">
+    <p>
+      Curent Price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
